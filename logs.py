bb2b7ed8635d2cabb2073383c068541c670abc1f
Zachary Caldwell <zachary.austin.caldwell@gmail.com>
2017-06-04 17:48:57 -0400

    Add function connect_to_database()

--- logs.py
+++ logs.py
@@ -1,8 +1,18 @@
 #!/bin/env/python3
 
-# 		------------- 	DESCRIPTION	 -------------
-# 	logs.py is an example of how to query a Postgresql database
-# 	to report data from a news website. The purpose is to obtain
-# 	useful information such as which authors attract the most views
-# 	or to find out which articles are the most popular 
-#
+#       -------------   DESCRIPTION  -------------
+#   logs.py is an example of how to query a Postgresql database
+#   to report data from a news website. The purpose is to obtain
+#   useful information such as which authors attract the most views
+#   or to find out which articles are the most popular 
+
+import psycopg2
+
+DBNAME = 'news'
+
+# A function to connect to the database
+def connect_to_database():
+    """ Connect to the database specified in the DBNAME constant """
+    database_object = psycopg2.connect(dbname=DBNAME)
+    cursor = database_object.cursor()
+    return cursor
