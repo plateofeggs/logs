97686da0aa4237cb7fb916e6a1345d1fc662dfa0
Zachary Caldwell <zachary.austin.caldwell@gmail.com>
2017-06-04 17:07:50 -0400

    Initialize repository with README and logs.py

---
+++ README.md
@@ -0,0 +1,9 @@
+# Example Logs Analysis Program
+
+## HOW TO RUN
+
+### Step 1 - Install Python 3
+
+This program uses Python 3. If you do not have python 3 installed, you should
+head over to [python.org/downloads](https://www.python.org/downloads/) and get
+yourself set up.
---
+++ logs.py
@@ -0,0 +1,8 @@
+#!/bin/env/python3
+
+# 		------------- 	DESCRIPTION	 -------------
+# 	logs.py is an example of how to query a Postgresql database
+# 	to report data from a news website. The purpose is to obtain
+# 	useful information such as which authors attract the most views
+# 	or to find out which articles are the most popular 
+#
