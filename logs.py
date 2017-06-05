59f500c6f446aef8d0cdff828721fc93d222cd15
Zachary Caldwell <zachary.austin.caldwell@gmail.com>
2017-06-04 21:04:00 -0400

    Clean up formatting on top_three_articles_alltime()

--- logs.py
+++ logs.py
@@ -4,7 +4,7 @@
 #   logs.py is an example of how to query a Postgresql database
 #   to report data from a news website. The purpose is to obtain
 #   useful information such as which authors attract the most views
-#   or to find out which articles are the most popular 
+#   or to find out which articles are the most popular
 
 import psycopg2
 
@@ -27,7 +27,7 @@ def top_three_articles_alltime():
                                "order by count(path) desc limit 3"))
     rank = 1
     print("\n\t\tTOP 3 ARTICLES\n")
-    print("Rank \t|\tViews \t|\tArticle Name")
+    print("Rank\t|\tViews\t|\tArticle Name")
     print("-------------------------------------------------------")
 
     for article in top_three:
