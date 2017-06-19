15a20cc20161c3482fcd59dfee9b25b3f09ba081
Zach Caldwell <zcald016@gmail.com>
2017-06-19 02:25:42 -0400

    Add createviews.sql to improve user-friendliness

---
+++ createviews.sql
@@ -0,0 +1,36 @@
+CREATE VIEW article_views AS 
+ SELECT count(log.path) AS views,
+    log.path
+   FROM log
+  WHERE log.status = '200 OK'::text AND NOT log.path = '/'::text
+  GROUP BY log.path
+  ORDER BY (count(log.path)) DESC
+ LIMIT 3;
+ 
+ CREATE VIEW bad_rqst AS 
+SELECT date(log."time") AS day,
+  count(log.id) AS total
+ FROM log
+ WHERE log.status = '404 NOT FOUND'::text
+ GROUP BY (date(log."time"))
+ ORDER BY (date(log."time"));
+ 
+ CREATE VIEW good_rqst AS 
+ SELECT date(log."time") AS day,
+    count(log.id) AS total
+   FROM log
+  GROUP BY (date(log."time"))
+  ORDER BY (date(log."time"));
+ 
+ CREATE VIEW high_404_days AS
+ SELECT to_char(good_rqst.day::timestamp with time zone, 'Month dd, YYYY'::text) AS to_char,
+    trunc(bad_rqst.total::numeric * 100::numeric / good_rqst.total::numeric, 1) AS trunc
+   FROM good_rqst
+     JOIN bad_rqst ON good_rqst.day = bad_rqst.day
+  WHERE (bad_rqst.total::numeric * 100::numeric / good_rqst.total::numeric) > 1::numeric;
+
+
+
+
+ 
+ 
