bc749c78f385e8a6de5003b8320399f0cbdc4066
Zach Caldwell <zcald016@gmail.com>
2017-06-18 21:53:31 -0400

    Fix #2 to shebang line

--- logs.py
+++ logs.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env/python3
+#!/usr/bin/env python3
 #       -------------   DESCRIPTION  -------------
 #   logs.py is an example of how to query a Postgresql database
 #   to report data from a news website. The purpose is to obtain
